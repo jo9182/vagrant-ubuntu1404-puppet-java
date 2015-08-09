88a623e25919d87aae290f794718d684cce7b794
lucasjellema <lucasjellema@gmail.com>
2015-08-09 13:20:22 +0200

    Add swapfile
    Add configuration to puppet manifest for the creation of a swapfile -
    required for installing software

--- manifests/base.pp
+++ manifests/base.pp
@@ -57,6 +57,28 @@ exec { "apt-update":
 }
 Exec["apt-update"]	-> Package <| |>
 
+
+## from https://github.com/biemond/puppet/issues/44 to set a swap file that is required later on for installing software
+exec { "create swap file":
+command => "/bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=8192",
+creates => "/var/swap.1",
+}
+
+exec { "attach swap file":
+command => "/sbin/mkswap /var/swap.1 && /sbin/swapon /var/swap.1",
+require => Exec["create swap file"],
+unless => "/sbin/swapon -s | grep /var/swap.1",
+}
+
+#add swap file entry to fstab
+exec {"add swapfile entry to fstab":
+command => "/bin/echo >>/etc/fstab /var/swap.1 swap swap defaults 0 0",
+require => Exec["attach swap file"],
+user => root,
+unless => "/bin/grep '^/var/swap.1' /etc/fstab 2>/dev/null",
+}
+
+
 # Configure Git
 #exec { "setup-git-username":
 #	command		=> "/usr/bin/git config --global user.name '$my_full_name'",
